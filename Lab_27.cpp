257ffcf94b1be67d8d15f94c6fd59100da31f5c5
Malakai <poncemalakai@gmail.com>
2025-11-02 01:21:33 -0700

    more methods

--- Lab_27.cpp
+++ Lab_27.cpp
@@ -3,54 +3,6 @@
 #include <vector>
 using namespace std;
 
-int main() {
-    // declarations
-    map<string, tuple<int, string, string>> villagerColors;
-
-    // insert elements into the map
-    // note how the right-hand side of the assignment are the vector elements
-    villagerColors["Audie"] = {10, "Yellow", "Red"};
-    villagerColors["Raymond"] = {48, "Gray", "White"};
-    villagerColors.insert({"Marshal", {5, "White", "Black"}});
-
-
-    int choice;
-
-    do {
-        displayMenu();
-        cin >> choice;
-
-        switch(choice) {
-            // case 1:
-            //     displayAllVillagers(villagerColors);
-            //     break;
-            case 1:
-                increaseFriendshipLevel(villagerColors);
-                break;
-            case 2:
-                cout << "Exiting program." << endl;
-                break;
-            default:
-                cout << "Invalid choice. Please try again." << endl;
-        }
-    } while(choice != 3);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-return 0;
-}
 
 void displayMenu() {
     cout << "\n=== Villager Friendship Menu ===" << endl;
@@ -77,4 +29,77 @@ void increaseFriendshipLevel(map<string, tuple<int, string, string>> villagerCol
         cout << villagerName << " not found." << endl;
     }
 
+}
+
+
+void decreaseFriendshipLevel(map<string, tuple<int, string, string>> villagerColors) { //using the increse block
+    string villagerName;
+    cout << "Enter the name of the villager to decrease friendship level: ";
+    cin >> villagerName;
+
+    auto name = villagerColors.find(villagerName);
+    if (name != villagerColors.end()) {
+
+        int level = get<0>(name->second);
+        level -= 1; 
+
+        cout << villagerName << "'s new friendship level is: " << level << endl;
+    } else {
+        cout << villagerName << " not found." << endl;
+    }
+
+}
+
+
+
+
+int main() {
+    // declarations
+    map<string, tuple<int, string, string>> villagerColors;
+
+    // insert elements into the map
+    // note how the right-hand side of the assignment are the vector elements
+    villagerColors["Audie"] = {10, "Yellow", "Red"};
+    villagerColors["Raymond"] = {48, "Gray", "White"};
+    villagerColors.insert({"Marshal", {5, "White", "Black"}});
+
+
+    int choice;
+
+    do {
+        displayMenu();
+        cin >> choice;
+
+        switch(choice) {
+            // case 1:
+            //     displayAllVillagers(villagerColors);
+            //     break;
+            case 1:
+                increaseFriendshipLevel(villagerColors);
+                break;
+            case 2:
+                decreaseFriendshipLevel(villagerColors);
+                break;
+            case 3:
+                cout << "Exiting program." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    } while(choice != 4);
+
+
+return 0;
+}
+
+
+void searchVilager(map<string, tuple<int, string, string>> villagerColors) {
+    string villagerName;
+    cout << "Enter the name of the villager to find: ";
+    cin >> villagerName;
+
+    name = auto villagerColors.find(villagerName);
+    if (name != villagerColors.end()) {
+        cout << villagerName << " found!" << endl;
+    }
 }
